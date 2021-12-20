a2a4bacbb4c79b602877b1f75d14eeb8b5d55c76
Marvin Ren <marvin.ren@yale.edu>
2021-12-20 00:57:55 -0500

    dist

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const webpack = require("webpack");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+
+module.exports ={
+    entry: {
+        app: "./assets/js/script.js",
+        events: "./assets/js/events.js",
+        schedule: "./assets/js/schedule.js",
+        tickets: "./assets/js/tickets.js"
+    },
+    output: {
+        path: __dirname + 'dist',
+        filename: '[name].bundle.js'
+    },
+    plugins:[
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery"
+        }),
+        new BundleAnalyzerPlugin({
+            analyzeMode: "static", // the report outputs to an HTML file in the dist folder
+        })
+    ],
+    mode: 'development'
+}
